324f7c798e9a7e547191d8eb23c83e0f5d84c35f
Pedro Fernandes <118320987+pedrofernandxs@users.noreply.github.com>
2024-02-18 15:46:55 -0300

    update deploy

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -89,7 +89,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-cmzw.onrender.com")
     websocket.onmessage = processMessage
 
 }
